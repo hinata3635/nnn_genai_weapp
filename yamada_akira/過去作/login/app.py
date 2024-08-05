37d5f1bcb1cb3674ed3774539b1e41b9bd2f1c0c
Pisces0128 <0128akira@gmail.com>
2024-08-05 21:57:03 +0900

    ソース修正

--- yamada_akira/過去作/login/app.py
+++ yamada_akira/過去作/login/app.py
@@ -4,14 +4,14 @@ import sqlite3
 
 
 app = Flask(__name__)
-# データベースの種類をファイル名を記載
+"""データベースの種類をファイル名を記載"""
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/git/AI/nnn_genai_weapp/yamada_akira/login/login.db'
-#データベースでイベントシステムがSQLAlchemyのセッションへの変更を追跡するのを無効化（リソースの節約）
+"""データベースでイベントシステムがSQLAlchemyのセッションへの変更を追跡するのを無効化（リソースの節約）"""
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#SQLAlchemyのインスタンスを作成して変数dbに割り当て
+"""SQLAlchemyのインスタンスを作成して変数dbに割り当て"""
 db = SQLAlchemy(app)
 
-#dbのModelを継承したテーブルのクラスを定義
+"""dbのModelを継承したテーブルのクラスを定義"""
 class Login(db.Model):
 	id = db.Column(db.String(128), primary_key=True)
 	password = db.Column(db.String(128), nullable=False)
@@ -23,8 +23,8 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add():
-	# id = request.form['chatbot-text']
-	# password = request.form['chatbot-text2']
+	"""id = request.form['chatbot-text']
+	   password = request.form['chatbot-text2']""" 
 	id = request.form['id']
 	password = request.form['password']
 	new_login = Login(id=id,password=password)
@@ -36,11 +36,13 @@ def add():
 def search():
 	id_input = request.args.get('searchId')
 	password_input = request.args.get('searchPassword')
-# 	if id_input is None or len(id_input) == 0:
-# +         data = Login.query.all()
-# +   else:
-# +         data = db.session.query(Login).filter(Login.id.like(id_input)).all()
-#     return render_template('login.html', data=data)
+	"""
+	if id_input is None or len(id_input) == 0:
++         data = Login.query.all()
++   else:
++         data = db.session.query(Login).filter(Login.id.like(id_input)).all()
+    return render_template('login.html', data=data)
+	"""
 
 if __name__ == '__main__':
 	app.run()
