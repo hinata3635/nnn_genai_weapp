4d809c033124dac1c0204589d431fc7a36f0742a
nagami-hinata <nagami.hinata@threen.jp>
2024-08-05 22:50:17 +0900

    made main file

---
+++ main/login2/app.py
@@ -0,0 +1,207 @@
+# from flask import Flask, render_template, redirect, url_for, request, flash, session
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField
+# from wtforms.validators import DataRequired
+# from datetime import datetime, timedelta
+# import os
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = os.urandom(24)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=30)
+# app.permanent_session_lifetime = timedelta(days=30)
+
+# # Flask-Loginの初期化
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
+# db = SQLAlchemy(app)
+
+# # Userモデルの定義 (UserMixinを継承)
+# class User(UserMixin, db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), unique=True, nullable=False)
+#     password_hash = db.Column(db.String(128), nullable=False)
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+#     @classmethod
+#     def create_new_user(cls, username, password):
+#         """
+#         新規ユーザーを登録するクラスメソッド。
+        
+#         :param username: 新規ユーザーのユーザー名
+#         :param password: 新規ユーザーのパスワード
+#         :return: Userオブジェクト（登録成功時）またはNone（ユーザー名が既に存在する場合）
+#         """
+
+#         # 既に同じユーザー名のユーザーが存在するかチェック
+#         existing_user = cls.query.filter_by(username=username).first()
+#         if existing_user is not None:
+#             return None  # 既に存在する場合はNoneを返して登録しない
+        
+#         # 新規ユーザーの作成
+#         new_user = cls(username=username)
+#         new_user.set_password(password)  # パスワードのハッシュ化
+#         db.session.add(new_user)
+#         db.session.commit()
+#         return new_user  # 新規ユーザーオブジェクトを返す
+
+# # Flask-Login用のユーザーローダー関数
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
+
+# # ログインフォームの定義 (変更なし)
+# class LoginForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     submit = SubmitField('Login')
+
+# # ログインのルート（Flask-Loginに対応）
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():  
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user and user.check_password(form.password.data):
+#             login_user(user, remember=True)  # Flask-Loginのlogin_user関数を使用
+#             flash('Login Successful!', 'success')
+#             next_page = request.args.get('next')
+#             return redirect(next_page or url_for('index'))
+#         else:
+#             flash('Login Unsuccessful. Please check username and password', 'danger')
+#     return render_template('login.html', title='Login', form=form)
+
+# # ログアウトのルート（Flask-Loginに対応）
+# @app.route('/logout')
+# def logout():
+#     logout_user()  # Flask-Loginのlogout_user関数を使用
+#     return redirect(url_for('login'))
+
+# # Flask-Loginのlogin_requiredデコレータを使用するルート
+# @app.route('/')
+# @login_required #<---------------------------------これをログインが必要なルートへ記述する
+# def index():
+#     return render_template("index.html")
+
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()  # データベースの初期化
+#         User.create_new_user("hoge","hogehoge") #　ログイン用のユーザーを追加する
+#     app.run(host="0.0.0.0", port=80, debug=False)
+
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+from datetime import datetime, timedelta
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=30)
+app.permanent_session_lifetime = timedelta(days=30)
+
+# Flask-Loginの初期化
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+db = SQLAlchemy(app)
+
+# Userモデルの定義 (UserMixinを継承)
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @classmethod
+    def create_new_user(cls, username, password):
+        existing_user = cls.query.filter_by(username=username).first()
+        if existing_user is not None:
+            return None
+        
+        new_user = cls(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+# Flask-Login用のユーザーローダー関数
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# ログインフォームの定義
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+# 登録フォームの定義
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=True)
+            flash('Login Successful!', 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User.create_new_user(form.username.data, form.password.data)
+        if new_user:
+            flash('Registration Successful! Please log in.', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('Username already exists. Please choose a different one.', 'danger')
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=80, debug=False)
